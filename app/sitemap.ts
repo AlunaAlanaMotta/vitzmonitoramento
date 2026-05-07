0fe42097a39dae4befe23abb8fce70f5937e713e
Alana Motta <mottaalana0@gmail.com>
2026-05-07 15:12:21 -0300

    atualizando sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,8 +1,12 @@
-export default function sitemap() {
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://seudominio.com",
+      url: "https://www.vitzmonitoramento.com.br",
       lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
     },
   ];
 }
